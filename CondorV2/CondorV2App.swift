b9ac8b406c2114bc02c831ab4334ce4c3eba739e
Jaakko Lintula <jaakkolintula6@gmail.com>
2024-10-31 15:10:22 +0200

    Initial Commit

---
+++ CondorV2/CondorV2App.swift
@@ -0,0 +1,17 @@
+//
+//  CondorV2App.swift
+//  CondorV2
+//
+//  Created by Jaakko Lintula on 31.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct CondorV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
